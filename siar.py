fba8f3c9bab276fe527235c900cc87c6a59e1dba
simon <simon.matern94@gmail.com>
2023-05-19 00:06:25 +0200

    make data generation more customizable

--- siar.py
+++ siar.py
@@ -10,6 +10,8 @@ import os
 import sys
 
 
+# HACK to import additional scripts 
+# https://docs.blender.org/api/current/info_tips_and_tricks.html
 full_path = os.path.realpath(__file__)
 path,_ = os.path.split(full_path)
 filename = "materials.py"
@@ -62,10 +64,8 @@ def get_homography(camera1, camera2):
     return H
 
 
-    
-    
-    
-def generate_sequence(ids, src="wikiart/", dst = "SIAR/", n_cameras = 50 ):
+
+def generate_dataset_wikiart(src="wikiart/", dst = "SIAR/",  ids=None,  **kwargs ):
 
     images_paths = glob.glob(src+"*/*")
     images_paths.sort()
@@ -76,38 +76,37 @@ def generate_sequence(ids, src="wikiart/", dst = "SIAR/", n_cameras = 50 ):
         for i,item in enumerate(images_paths):
             # write each item on a new line
             fp.write("%d:%s\n" % (i,item))
+    
+    if ids is None:
+        ids = [len(imgs)]
 
     for i in range(*ids):
-        print(images_paths[i])
-        
-        #if os.path.exists(dst+str(i)):
-        #    continue
-
-        add_plane(dirs[i]+"/"+imgs[i])
+        print("Rendering:", images_paths[i])
+        generate_sequence(dirs[i]+"/"+imgs[i], f"{dst}/{i}/", **kwargs)
 
 
-        
-        x,y = bpy.data.images['painting'].size
-        #if x*y >1000000:
-            #print("Resolution too high!")
-            #continue
-        
 
+    
+def generate_sequence(img_file, dst = "distortions/", n_cameras = 50, homography=False ):
+    
+    add_plane(img_file)
+    if homography:
         add_cameras(n_cameras)
-        add_front_camera("Camera0")
-        render_ambient("Camera0", dst+str(i)+"/gt.png")
+    add_front_camera("Camera0")
+    render_ambient("Camera0", dst+"/gt.png")
 
+    if homography:
         for cam_i in range(n_cameras+1):
             for cam_j in range(cam_i+1, n_cameras+1):
                 H_ij = get_homography(f"Camera{cam_i}",f"Camera{cam_j}")
-                np.savetxt(dst+str(i)+f"/H_{cam_i}_{cam_j}.mat", H_ij)
-        
-        for j in range(n_cameras+1):
-
-            generate_scene(dirs[i]+"/"+imgs[i], [2,5], [3])
-
-            render("Camera"+str(j), dst+str(i)+"/{}.png".format(j))
-            render_mask("Camera"+str(j), dst+str(i)+"/{}-m.png".format(j))
+                np.savetxt(dst+f"/H_{cam_i}_{cam_j}.mat", H_ij)
+    
+    for j in range(n_cameras+1):
+        generate_scene(img_file, [2,5], [3])
+        camera = f"Camera{j}" if homography else "Camera0"
+        render(camera, dst +"/{}.png".format(j))
+        render_mask(camera, dst +"/{}-m.png".format(j))
+    
 
 
 
@@ -138,38 +137,18 @@ def generate_sequence_aligned(ids, src="wikiart/",  dst =  "DIAR/", n_cameras = 
         
         if os.path.exists(dst+str(i)):
             continue
-        try:
+
+        add_plane(dirs[i]+"/"+imgs[i])
+        add_front_camera("Camera0")
+        render_ambient("Camera0", dst+str(i)+"/gt.png")
+
+        
+        for j in range(n_cameras+1):
             add_plane(dirs[i]+"/"+imgs[i])
+            generate_scene(dirs[i]+"/"+imgs[i], [2,5], [3])
+            render("Camera0", dst+str(i)+"/{}.png".format(j))
+            render_mask("Camera0", dst+str(i)+"/{}-m.png".format(j))
 
-            
-            x,y = bpy.data.images['painting'].size
-            #if x*y >1000000:
-                #print("Resolution too high!")
-                #continue
-            
-
-            #add_cameras(n_cameras)
-            add_front_camera("Camera0")
-
-            render_ambient("Camera0", dst+str(i)+"/gt.png")
-
-            
-            for j in range(n_cameras+1):
-                add_plane(dirs[i]+"/"+imgs[i])
-
-                n_lights = np.random.randint(1, 5)
-                add_illumination(n_lights)
-                n_objects = np.random.randint(0,3)
-                add_objects(n_objects)
-                
-
-                render("Camera0", dst+str(i)+"/{}.png".format(j))
-                render_mask("Camera0", dst+str(i)+"/{}-m.png".format(j))
-
-
-        except Exception as e:
-            print(e)
-            print("---------Error in image {} ----------".format(i))
                 
 
 def project_3d_point(camera: bpy.types.Object,
@@ -232,19 +211,7 @@ bpy.context.scene.render.engine = 'CYCLES'
 print(sys.argv)
 n = 50010
 #generate_image((n,n+1000), "DIAR-Eval/")    
-generate_sequence((n,n+1), dst="test2/", n_cameras=5)    
-generate_sequence_aligned((50011,50012), dst="test2/", n_cameras= 5)  
-
-add_plane("wikiart/Romanticism/lev-lagorio_crimean-landscape-1891.jpg")
-add_illumination(2)
-add_cameras(3)
-add_front_camera()
-#render_ambient(image_path="vis/gt.png")
-add_objects(3)
-
-render("Camera2", image_path="test/before-mask.png")
-
-render_mask("Camera2", image_path="test/after-mask.png")
-#render_ambient()
+generate_dataset_wikiart(src="wikiart/", dst="test2/", ids= (n,n+1), n_cameras=5, homography=True)    
+generate_dataset_wikiart(src="wikiart/", dst="test2/", ids= (n+1,n+2), n_cameras=5, homography=False)    
 
 
